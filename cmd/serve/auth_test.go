5d159ac16dec017e8da790e3cdada5ee1afd6b5c
Tobias Salzmann <796084+Eun@users.noreply.github.com>
2022-11-22 14:52:26 +0100

    fix test

--- cmd/serve/auth_test.go
+++ cmd/serve/auth_test.go
@@ -9,41 +9,41 @@ import (
 
 func TestCrypt(t *testing.T) {
 	tests := []struct {
-		in           string
+		in           []byte
 		keyToEncrypt string
 		keyToDecrypt string
 		wantOk       bool
 	}{
 		{
-			in:           "hello",
+			in:           []byte("hello"),
 			keyToEncrypt: "password",
 			keyToDecrypt: "password",
 			wantOk:       true,
 		},
 		{
-			in:           "world",
+			in:           []byte("world"),
 			keyToEncrypt: "password",
 			keyToDecrypt: "password",
 			wantOk:       true,
 		},
 		{
-			in:           "",
+			in:           nil,
 			keyToEncrypt: "password",
 			keyToDecrypt: "password",
 			wantOk:       true,
 		},
 		{
-			in: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			in: []byte(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
 labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
 commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
 pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-laborum.`,
+laborum.`),
 			keyToEncrypt: "password",
 			keyToDecrypt: "password",
 			wantOk:       true,
 		},
 		{
-			in:           "Hello",
+			in:           []byte("Hello"),
 			keyToEncrypt: "password1",
 			keyToDecrypt: "password2",
 			wantOk:       false,
